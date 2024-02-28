4d99c5e802a58425984e91f2200fbd70fab34283
Merzlikin-Matvey <merzlikinmatvey01@gmail.com>
2024-02-29 00:03:36 +0300

    Обновил gitignore

--- main.cpp
+++ main.cpp
@@ -7,6 +7,6 @@ using namespace std;
 
 int main() {
     Board board;
-    board.import_json("C:\\Users\\merzl\\CLionProjects\\chess8\\saves\\data.json");
+    board.import_json("C:\\Users\\User\\CLionProjects\\chess6\\saves\\data.json");
     board.print();
 }

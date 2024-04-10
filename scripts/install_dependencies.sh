dc10be3c48bf0da7ea25341fed50fa27d722c825
Merzlikin-Matvey <merzlikinmatvey01@gmail.com>
2024-04-10 23:40:13 +0300

    Минор

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-repos=($(jq -r '.dependencies[]' dependencies.json))
+# If dependencies.json file does not exist, exit with error
+if [ ! -f ../dependencies.json ]; then
+    echo "File dependencies.json does not exist"
+    exit 1
+fi
+
+# Read dependencies.json file and get all dependencies
+repos=($(jq -r '.dependencies[]' ../dependencies.json))
 
 for repo in "${repos[@]}"; do
     IFS='/' read -r -a array <<< "$repo"
@@ -9,20 +16,21 @@ for repo in "${repos[@]}"; do
     branch_name=${array[2]}
     file_name=${array[-1]}
 
-    path=../include/$repo_name
+    path=../dependencies/$repo_name
 
     if [ -d $path ]; then
         rm -rf $path
     fi
 
+    # Download the file from the repository
     mkdir -p $path
     curl -LO https://raw.githubusercontent.com/$repo;
     mv $file_name $path
 
     if [ $? -eq 0 ]; then
-        echo "Установка $repo_name прошла успешно"
+        echo "$repo_name installed successfully"
     else
-        echo "Ошибка при установке $repo_name"
+        echo "Failed to install $repo_name"
         exit 1
     fi
 done

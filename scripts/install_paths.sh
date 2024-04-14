0a7f47145855f67f057afb1f66c841f5e0f0a4b7
Matvey Merzlikin <merzlikinmatvey01@gmail.com>
2024-04-14 23:14:48 +0300

    Переменные среды теперь нормально ставятся и на Linux

--- scripts/install_paths.sh
+++ scripts/install_paths.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-# Проверяем, установлена ли переменная окружения
-if [ -n "$CHESS_ENGINE_RESOURCE_PATH" ]
+if [ -n "$CHESS_ENGINE_PATH" ]
 then
-    # Если переменная установлена, удаляем ее из .bashrc
-    grep -v 'export CHESS_ENGINE_RESOURCE_PATH' ~/.bashrc > temp && mv temp ~/.bashrc
-    echo "Removed CHESS_ENGINE_RESOURCE_PATH"
+    grep -v 'export CHESS_ENGINE_PATH' ~/.bashrc > temp && mv temp ~/.bashrc
+    grep -v 'export PATH' ~/.bashrc > temp && mv temp ~/.bashrc
 fi
 
-# Если был передан аргумент, устанавливаем новое значение переменной
 if [ -n "$1" ]
 then
-    echo "export CHESS_ENGINE_RESOURCE_PATH=\"$1\"" >> ~/.bashrc
-    source ~/.bashrc
+    export CHESS_ENGINE_PATH="$1"
+    echo "export CHESS_ENGINE_PATH=\"$1\"" >> ~/.bashrc
+    echo "export PATH=\$PATH:\$CHESS_ENGINE_PATH/lib" >> ~/.bashrc
 
-    if [ -z "$CHESS_ENGINE_RESOURCE_PATH" ]
+    if [ -z "$CHESS_ENGINE_PATH" ]
     then
-        echo "Failed to set CHESS_ENGINE_RESOURCE_PATH"
+        echo "Failed to set CHESS_ENGINE_PATH"
         exit 1
     fi
 fi

a240f794ecc6514266c00633fad1a3fdfd5a5900
Merzlikin-Matvey <merzlikinmatvey01@gmail.com>
2024-05-08 23:07:49 +0300

    Фикс пин масок

---
+++ include/headers/pin_mask_magic_numbers.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <array>
+#include <iostream>
+#include <memory>
+
+typedef uint64_t Bitboard;
+
+extern "C" {
+    #include "C functions/magic_numbers.h"
+}
+
+namespace chess::magic_numbers {
+    Bitboard generate_vertical_pin_magic_number(uint8_t square){
+        return _generate_vertical_pin_magic_number(square);
+    }
+
+    std::array<Bitboard, 64> generate_vertical_pin_magic_numbers() {
+        std::array<Bitboard, 64> magic_numbers{};
+        for (uint8_t square = 0; square < 64; square++) {
+            magic_numbers[square] = generate_vertical_pin_magic_number(square);
+        }
+        return magic_numbers;
+    }
+
+    void fancy_print_vertical_pin_magic_numbers() {
+        _fancy_print_vertical_pin_magic_numbers();
+    }
+
+    Bitboard generate_horizontal_pin_magic_number(uint8_t square){
+        return ::_generate_horizontal_pin_magic_number(square);
+    }
+
+    std::array<Bitboard, 64> generate_horizontal_pin_magic_numbers() {
+        std::array<Bitboard, 64> magic_numbers{};
+        for (uint8_t square = 0; square < 64; square++) {
+            magic_numbers[square] = generate_horizontal_pin_magic_number(square);
+        }
+        return magic_numbers;
+    }
+
+    void fancy_print_horizontal_pin_magic_numbers() {
+        _fancy_print_horizontal_pin_magic_numbers();
+    }
+
+    Bitboard generate_up_right_pin_magic_number(uint8_t square){
+        return ::_generate_up_right_pin_magic_number(square);
+    }
+
+    std::array<Bitboard, 64> generate_up_right_pin_magic_numbers() {
+        std::array<Bitboard, 64> magic_numbers{};
+        for (uint8_t square = 0; square < 64; square++) {
+            magic_numbers[square] = generate_up_right_pin_magic_number(square);
+        }
+        return magic_numbers;
+    }
+
+    void fancy_print_up_right_pin_magic_numbers() {
+        _fancy_print_up_right_pin_magic_numbers();
+    }
+
+    Bitboard generate_down_right_pin_magic_number(uint8_t square){
+        return ::_generate_down_right_pin_magic_number(square);
+    }
+
+    std::array<Bitboard, 64> generate_down_right_pin_magic_numbers() {
+        std::array<Bitboard, 64> magic_numbers{};
+        for (uint8_t square = 0; square < 64; square++) {
+            magic_numbers[square] = generate_down_right_pin_magic_number(square);
+        }
+        return magic_numbers;
+    }
+
+    void fancy_print_down_right_pin_magic_numbers() {
+        _fancy_print_down_right_pin_magic_numbers();
+    }
+}

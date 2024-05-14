678018c63d03b87f7e1830eec3c8fa2d2ad20853
Matvey Merzlikin <merzlikinmatvey01@gmail.com>
2024-05-14 13:30:14 +0300

    Минор

--- include/chess-lib.hpp
+++ include/chess-lib.hpp
@@ -2,4 +2,21 @@
 
 
 #include "headers/board.hpp"
+#include "headers/bitboard_operations.hpp"
+#include "headers/zobrist.hpp"
+#include "headers/constants.hpp"
+#include "headers/fen.hpp"
+#include "headers/move.hpp"
+#include "headers/notations.hpp"
+#include "headers/bitboard_lines.hpp"
+#include "headers/magic_numbers.hpp"
+#include "headers/masks/bishop_masks.hpp"
+#include "headers/masks/vertical_pin_masks.hpp"
+#include "headers/masks/horizontal_pin_masks.hpp"
+#include "headers/masks/up_right_diagonal_pin_masks.hpp"
+#include "headers/masks/down_right_diagonal_pin_masks.hpp"
+#include "headers/pin_mask_magic_numbers.hpp"
+#include "headers/magic_numbers_constants.hpp"
+#include "headers/masks/pawn_masks.hpp"
+#include "headers/check_and_checkmate.hpp"
 

fc3c3592d19228b9dcabd5df9c41f06bd4543920
Merzlikin-Matvey <merzlikinmatvey01@gmail.com>
2024-05-29 20:42:29 +0300

    оп-оп-оп работает

--- scripts/wasm.sh
+++ scripts/wasm.sh
@@ -15,14 +15,17 @@ project_root_dir=$(pwd)
 src_dir="./src"
 include_dir="./include"
 
-src_files=$(find $src_dir -name "*.cpp")
+all_files=$(find $src_dir -name "*.cpp")
+src_files=$(echo "$all_files" | grep -v "$src_dir/wasm")
+wasm_files=$(echo "$all_files" | grep "$src_dir/wasm")
 header_files=$(find $include_dir -name "*.hpp")
 
 echo "Source files: $src_files"
+echo "WASM files: $wasm_files"
 echo "Header files: $header_files"
 
 output_dir="./wasm"
-output_file="chess_lib.wasm"
+output_file="chess_lib"
 
 if [ ! -d $output_dir ]
 then
@@ -31,4 +34,13 @@ else
     rm -rf $output_dir/*
 fi
 
-emcc $src_files -I $include_dir -o $output_dir/$output_file -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -std=c++23 -O3 -fconstexpr-steps=2147483647 -s SIDE_MODULE=1
+echo "Compile as a side module? [Y/n] "
+read answer
+
+if [ "$answer" = "Y" ] || [ "$answer" = "y" ]; then
+    echo "Compiling as a side module..."
+    emcc $src_files -I $include_dir -std=c++23 -s EXPORT_ALL=1 -fconstexpr-steps=2147483647 -s SIDE_MODULE=1 -o $output_dir/$output_file.js
+else
+    echo "Compiling as a main module..."
+    emcc $all_files -I $include_dir -std=c++23 -s EXPORT_ALL=1 -fconstexpr-steps=2147483647 -o $output_dir/$output_file.js -s EXTRA_EXPORTED_RUNTIME_METHODS='[cwrap]'
+fi

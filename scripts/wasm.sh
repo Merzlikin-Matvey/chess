588dd3b0544650d68e915580af8d33d4703802e0
Merzlikin-Matvey <merzlikinmatvey01@gmail.com>
2024-05-29 22:10:42 +0300

    Компиляция под WASM

--- scripts/wasm.sh
+++ scripts/wasm.sh
@@ -15,14 +15,11 @@ project_root_dir=$(pwd)
 src_dir="./src"
 include_dir="./include"
 
-all_files=$(find $src_dir -name "*.cpp")
-src_files=$(echo "$all_files" | grep -v "$src_dir/wasm")
-wasm_files=$(echo "$all_files" | grep "$src_dir/wasm")
-header_files=$(find $include_dir -name "*.hpp")
+src_files=$(find $src_dir -name "*.cpp" -not -path "$src_dir/wasm/*")
+wasm_files=$(find $src_dir/wasm -name "*.cpp")
 
 echo "Source files: $src_files"
 echo "WASM files: $wasm_files"
-echo "Header files: $header_files"
 
 output_dir="./wasm"
 output_file="chess_lib"
@@ -42,5 +39,5 @@ if [ "$answer" = "Y" ] || [ "$answer" = "y" ]; then
     emcc $src_files -I $include_dir -std=c++23 -s EXPORT_ALL=1 -fconstexpr-steps=2147483647 -s SIDE_MODULE=1 -o $output_dir/$output_file.js
 else
     echo "Compiling as a main module..."
-    emcc $all_files -I $include_dir -std=c++23 -s EXPORT_ALL=1 -fconstexpr-steps=2147483647 -o $output_dir/$output_file.js -s EXTRA_EXPORTED_RUNTIME_METHODS='[cwrap]'
+    emcc $src_files $wasm_files -I $include_dir -std=c++23 -s EXPORT_ALL=1 -fconstexpr-steps=2147483647 -o $output_dir/$output_file.js -s EXPORTED_RUNTIME_METHODS='["cwrap"]'
 fi

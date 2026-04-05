15276f8a4870caee811327ec975258320071b911
Merzlikin-Matvey <merzlikinmatvey01@gmail.com>
2026-04-06 01:17:11 +0300

    refactor(quiescence): optimize capture moves and enable LTO

--- src/ai/quiescence.cpp
+++ src/ai/quiescence.cpp
@@ -5,21 +5,32 @@
 double chess::engine::AI::quiescence_max(Board& board, double alpha, double beta) {
     nodes_searched++;
 
-    double standard_eval = evaluate_position(board, White);
+    const double standard_eval = evaluate_position(board, White);
     if (standard_eval >= beta) return beta;
     if (alpha < standard_eval) alpha = standard_eval;
 
+    constexpr double big_delta = 9;
+    if (standard_eval + big_delta < alpha) return alpha;
+
     auto moves = board.legal_moves;
 
+    // TODO: get_legal_captures
+    int capture_count = 0;
     for (int i = 0; i < moves.size(); i++) {
-        Move move = moves.moves[i];
-        if (move.second_type() == 255) continue;
+        if (moves.moves[i].second_type() != 255) {
+            moves.moves[capture_count++] = moves.moves[i];
+        }
+    }
+    if (capture_count == 0) return alpha;
+    moves.resize(capture_count);
+    sort_moves(&moves);
 
+    for (int i = 0; i < capture_count; i++) {
         PositionState state;
-        board.make_move(move, state);
+        board.make_move(moves.moves[i], state);
         board.get_legal_moves();
         double score = quiescence_min(board, alpha, beta);
-        board.unmake_move(move, state);
+        board.unmake_move(moves.moves[i], state);
 
         if (score >= beta) return beta;
         if (score > alpha) alpha = score;
@@ -35,17 +46,27 @@ double chess::engine::AI::quiescence_min(Board& board, double alpha, double beta
     if (standard_eval <= alpha) return alpha;
     if (beta > standard_eval) beta = standard_eval;
 
+    constexpr double big_delta = 9;
+    if (standard_eval - big_delta > beta) return beta;
+
+    // TODO: get_legal_captures
     auto moves = board.legal_moves;
-
+    int capture_count = 0;
     for (int i = 0; i < moves.size(); i++) {
-        Move move = moves.moves[i];
-        if (move.second_type() == 255) continue;
+        if (moves.moves[i].second_type() != 255) {
+            moves.moves[capture_count++] = moves.moves[i];
+        }
+    }
+    if (capture_count == 0) return beta;
+    moves.resize(capture_count);
+    sort_moves(&moves);
 
+    for (int i = 0; i < capture_count; i++) {
         PositionState state;
-        board.make_move(move, state);
+        board.make_move(moves.moves[i], state);
         board.get_legal_moves();
         double score = quiescence_max(board, alpha, beta);
-        board.unmake_move(move, state);
+        board.unmake_move(moves.moves[i], state);
 
         if (score <= alpha) return alpha;
         if (score < beta) beta = score;
